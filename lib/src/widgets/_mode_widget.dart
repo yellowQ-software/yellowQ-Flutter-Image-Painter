45b9cd2ac5e0060d7986c562351d23313b7224e3
Guillermo Mercado <guillermo.mercado@aumenta.mx>
2022-07-14 17:56:06 -0500

    Very specific personal needs, removed a paint mode

--- lib/src/widgets/_mode_widget.dart
+++ lib/src/widgets/_mode_widget.dart
@@ -70,10 +70,6 @@ List<ModeData> paintModes(TextDelegate textDelegate) => [
           icon: Icons.power_input,
           mode: PaintMode.dashLine,
           label: textDelegate.dashLine),
-      ModeData(
-          icon: Icons.text_format,
-          mode: PaintMode.text,
-          label: textDelegate.text),
     ];
 
 @immutable
